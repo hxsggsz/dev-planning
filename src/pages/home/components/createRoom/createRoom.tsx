e1d45927168ce8d4d65f74fa13090e75f07f3249
hxsggsz <vt.hugo.2021@gmail.com>
2024-01-15 01:10:56 -0300

    add path alias and initial home component

---
+++ src/pages/home/components/createRoom/createRoom.tsx
@@ -0,0 +1,12 @@
+import Input from "@/components/input/input";
+
+function CreateRoom() {
+  return (
+    <>
+      <h1>Dev Planning</h1>
+      <Input.Root></Input.Root>
+    </>
+  );
+}
+
+export default CreateRoom;
